a6a6feb04a3594c85614c9299cff85a59124275c
malt78 <123999485+malt78@users.noreply.github.com>
2023-09-16 23:55:23 -0500

    Add addition program to repository.

---
+++ sow_cpp_ch02/ch2_01.cpp
@@ -0,0 +1,17 @@
+/*
+  This program stores two integers
+  and their sum in a variable named total
+  and displays the total.
+*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int number_01 = 75;
+  int number_02 = 25;
+  int total = number_01 + number_02;
+
+  cout << total << endl;
+  return 0;
+}
