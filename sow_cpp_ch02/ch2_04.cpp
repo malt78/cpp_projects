48c56d5cb1200c3380eec626ac2d962872012e0f
malt78 <123999485+malt78@users.noreply.github.com>
2023-09-20 22:30:04 -0500

    Add Bill program.

---
+++ sow_cpp_ch02/ch2_04.cpp
@@ -0,0 +1,13 @@
+/*
+    This program calculates the tax 
+    and tip for $88.67.
+*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  double BILL_TOTAL = 88.67;
+  double TAX = 0.675;
+  double TIP =  // Tip is 20% after adding the tax.
+}
