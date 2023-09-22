1a4345e7e436e1cd18e79ac2d0ad08917c793c47
malt78 <123999485+malt78@users.noreply.github.com>
2023-09-21 23:27:52 -0500

    Update tip calculation  and add test statement.

--- sow_cpp_ch02/ch2_04.cpp
+++ sow_cpp_ch02/ch2_04.cpp
@@ -9,5 +9,9 @@ int main()
 {
   double BILL_TOTAL = 88.67;
   double TAX = 0.675;
-  double TIP =  // Tip is 20% after adding the tax.
+  double TIP = 0.20 * (BILL_TOTAL * TAX); // Tip is 20% after adding the tax.
+
+  // Test statement for correct output
+  cout << "$" << TIP << endl;
+
 }
